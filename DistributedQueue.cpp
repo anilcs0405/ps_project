fbcd77ebc18594e915445eeb27f9b782daf6921b
Anil <anil@cs.utexas.edu>
2011-05-07 03:11:41 -0500

    "One"

---
+++ DistributedQueue.cpp
@@ -0,0 +1,31 @@
+#include <mpi.h>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <map>
+#include <list>
+#include <ctime>
+#include <string.h>
+#include <math.h>
+#include <fstream>
+
+#include "FileIO.h"
+#include "Queue.h"
+#include "DistributedQueue.h"
+
+using namespace std;
+
+DistributedQueue::DistributedQueue(){
+	
+}
+
+DistributedQueue::~DistributedQueue(){
+
+}
+
+void DistributedQueue::ProcessFunction(void *pid)
+{
+	
+
+}
+
