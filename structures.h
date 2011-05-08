6bb4cd5fb1fca860de9dda7c45b36a22d45db2fa
Anil <anil@cs.utexas.edu>
2011-05-08 00:39:07 -0500

    structures

---
+++ structures.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <list>
+#include <pthread.h>
+
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+using namespace std;
+
+// work item inserted into the queue
+
+struct work_item {
+	unsigned short load;
+	char *file_name;
+	bool is_spam;
+	work_item *next;
+};
+
+typedef struct work_item work_item;
+
+struct multiple_args{
+        void *obj;
+        void *tid;
+};
+
+typedef struct multiple_args m_args;
+
+#endif
